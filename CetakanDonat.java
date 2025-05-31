452b388b94fced11a7d4e99fe0fca00e4cab1779
naylashafa <naylaashafa@student.ub.ac.id>
2025-05-31 20:54:43 +0700

    Add files via upload

---
+++ CetakanDonat.java
@@ -0,0 +1,31 @@
+package uap.bases;
+
+public abstract class CetakanDonat {
+    protected static final int pi_num = 22;
+    protected static final int pi_deno = 7;
+    protected static final double tebal = 0.5;
+    protected static final double densitas = 8.0;
+
+    public abstract double getVolume();
+    public abstract double getLuasPermukaan();
+
+    public double getMassa() {
+        return getVolume() * tebal * densitas;
+    }
+
+    public double getMassaKg() {
+        return getMassa() / 1000;
+    }
+
+    public int getBiayaKirim() {
+        return (int) Math.ceil(getMassaKg()) * 2000;
+    }
+
+    public void printInfo() {
+        System.out.printf("Volume          : %.2f\n", getVolume());
+        System.out.printf("Luas permukaan  : %.2f\n", getLuasPermukaan());
+        System.out.printf("Massa           : %.2f\n", getMassa());
+        System.out.printf("Massa dalam kg  : %.2f\n", getMassaKg());
+        System.out.printf("Biaya kirim     : Rp%d\n", getBiayaKirim());
+    }
+}
